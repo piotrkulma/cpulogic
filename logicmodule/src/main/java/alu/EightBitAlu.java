c54855285e74d7a40385d2c9ecd553b2ebab639a
peter <piotrkulma@gmail.com>
2014-10-04 18:05:39 +0200

    Implementacja EIghtBitAlu. Testy części arytmetycznej EightBitAlu

---
+++ logicmodule/src/main/java/alu/EightBitAlu.java
@@ -0,0 +1,59 @@
+package alu;
+
+import logic.LogicValue;
+
+/**
+ * Created by Piotr Kulma on 04.10.14.
+ */
+public class EightBitAlu {
+    public final static int BIT_COUNT = 8;
+
+    private LogicValue[] inputA;
+    private LogicValue[] inputB;
+    private LogicValue[] operationCode;
+
+    private OneBitAlu[] aluArray;
+
+    public EightBitAlu(LogicValue[] operCode, LogicValue[] a, LogicValue[] b) {
+        this.operationCode = operCode;
+        this.inputA = a;
+        this.inputB = b;
+        initArchitecture();
+    }
+
+    public LogicValue[] getOutputValue() {
+        LogicValue carryIn = new LogicValue(LogicValue.LOGIC_VALUE_ZERO);
+        LogicValue[] output = new LogicValue[BIT_COUNT];
+
+        for(int i=0; i<BIT_COUNT; i++) {
+            aluArray[i] = new OneBitAlu(operationCode, new LogicValue(carryIn.getValue()), inputA[i], inputB[i]);
+            carryIn = aluArray[i].getCarryOutput();
+            output[i] = new LogicValue(aluArray[i].getOperationOutput().getValue());
+        }
+
+        return output;
+    }
+
+    public LogicValue getCarryValue() {
+        return aluArray[BIT_COUNT-1].getCarryOutput();
+    }
+
+    public void initArchitecture() {
+        aluArray = new OneBitAlu[BIT_COUNT];
+
+        refresh(operationCode, inputA, inputB);
+    }
+
+    public void refresh(LogicValue[] operCode, LogicValue[] a, LogicValue[] b) {
+        this.operationCode = operCode;
+        this.inputA = a;
+        this.inputB = b;
+
+        LogicValue carryIn = new LogicValue(LogicValue.LOGIC_VALUE_ZERO);
+
+        for(int i=0; i<BIT_COUNT; i++) {
+            aluArray[i] = new OneBitAlu(operCode, carryIn, a[i], b[i]);
+            carryIn = aluArray[i].getCarryOutput();
+        }
+    }
+}
---
+++ logicmodule/src/test/java/EightBitAluTest.java
@@ -0,0 +1,163 @@
+import alu.EightBitAlu;
+import logic.LogicValue;
+import logic.utils.LogicValueUtil;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by Piotr Kulma on 04.10.14.
+ */
+
+public class EightBitAluTest {
+    private LogicValue[] a = LogicValueUtil.getLogicValueArrayFromString("00000000");
+    private LogicValue[] b = LogicValueUtil.getLogicValueArrayFromString("00000000");
+    private LogicValue[] operCode = LogicValueUtil.getLogicValueArrayFromString("000");
+
+    private EightBitAlu eightBitAlu = null;
+
+    @Test
+    public void eightBitAluORTest() {
+        setValues(OneBitAluTest.OR_OPERATION_CODE_STRING, "10101010", "01010101");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.OR_OPERATION_CODE_STRING, "11110000", "00001111");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.OR_OPERATION_CODE_STRING, "11111111", "00000000");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.OR_OPERATION_CODE_STRING, "00000000", "11111111");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.OR_OPERATION_CODE_STRING, "00000000", "00000000");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.OR_OPERATION_CODE_STRING, "11110000", "00000000");
+        Assert.assertEquals("11110000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.OR_OPERATION_CODE_STRING, "10101010", "00000000");
+        Assert.assertEquals("10101010", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.OR_OPERATION_CODE_STRING, "00000000", "01010101");
+        Assert.assertEquals("01010101", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.OR_OPERATION_CODE_STRING, "11111111", "11111111");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.OR_OPERATION_CODE_STRING, "10101010", "10101010");
+        Assert.assertEquals("10101010", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+    }
+
+    @Test
+    public void eightBitAluANDTest() {
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "11111111", "11111111");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "10101010", "10101010");
+        Assert.assertEquals("10101010", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "11111111", "00000000");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "00000000", "11111111");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "00000000", "00000000");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "11110000", "00000000");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "10101010", "00000000");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "00000000", "01010101");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "10101010", "01010101");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "11110000", "11110000");
+        Assert.assertEquals("11110000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "00001111", "00001111");
+        Assert.assertEquals("00001111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.AND_OPERATION_CODE_STRING, "01010101", "01010101");
+        Assert.assertEquals("01010101", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+    }
+
+    @Test
+    public void eightBitAluNOTTest() {
+        setValues(OneBitAluTest.NOT_OPERATION_CODE_STRING, "11111111", "00000000");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.NOT_OPERATION_CODE_STRING, "00000000", "00000000");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.NOT_OPERATION_CODE_STRING, "10101010", "00000000");
+        Assert.assertEquals("01010101", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.NOT_OPERATION_CODE_STRING, "11110000", "00000000");
+        Assert.assertEquals("00001111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.NOT_OPERATION_CODE_STRING, "00001111", "00000000");
+        Assert.assertEquals("11110000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.NOT_OPERATION_CODE_STRING, "11111111", "11111111");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.NOT_OPERATION_CODE_STRING, "00000000", "11111111");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.NOT_OPERATION_CODE_STRING, "10101010", "11111111");
+        Assert.assertEquals("01010101", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.NOT_OPERATION_CODE_STRING, "11110000", "11111111");
+        Assert.assertEquals("00001111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.NOT_OPERATION_CODE_STRING, "00001111", "11111111");
+        Assert.assertEquals("11110000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+    }
+
+    @Test
+    public void eightBitAluXORTest() {
+        setValues(OneBitAluTest.XOR_OPERATION_CODE_STRING, "11111111", "00000000");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.XOR_OPERATION_CODE_STRING, "00000000", "11111111");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.XOR_OPERATION_CODE_STRING, "11111111", "11111111");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.XOR_OPERATION_CODE_STRING, "10101010", "01010101");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.XOR_OPERATION_CODE_STRING, "10101010", "10101010");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.XOR_OPERATION_CODE_STRING, "01010101", "01010101");
+        Assert.assertEquals("00000000", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.XOR_OPERATION_CODE_STRING, "11110000", "00001111");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+
+        setValues(OneBitAluTest.XOR_OPERATION_CODE_STRING, "00001111", "11110000");
+        Assert.assertEquals("11111111", LogicValueUtil.getLogicValueArrayToString(eightBitAlu.getOutputValue()));
+    }
+
+    @Ignore
+    public void setValues(String opCodeStr, String a, String b) {
+        LogicValueUtil.setLogicValueArrayFromString(opCodeStr, operCode);
+        LogicValueUtil.setLogicValueArrayFromString(a, this.a);
+        LogicValueUtil.setLogicValueArrayFromString(b, this.b);
+
+        if(eightBitAlu == null) {
+            eightBitAlu = new EightBitAlu(operCode, this.a, this.b);
+        }
+
+        eightBitAlu.refresh(operCode, this.a, this.b);
+    }
+}
